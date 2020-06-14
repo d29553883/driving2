fa9b06122ce59f3ab8413ec04bc657973b8a823b
David Lin <d2955388365@gmail.com>
2020-06-14 21:15:54 +0800

    add us

--- driving2.py
+++ driving2.py
@@ -3,7 +3,13 @@ age = input('幾歲 :')
 age = int(age)
 if country == '台灣': 
    if age >= 18 :
-   	print('你可以考駕照')
+   	   print('你可以考駕照')
    else:
-   	print('不能考駕照')
+   	   print('不能考駕照')
+elif country == '美國':
+   if age >= 16 :
+       print('你可以考駕照')
+   else :
+       print('不能考駕照')
+
     
